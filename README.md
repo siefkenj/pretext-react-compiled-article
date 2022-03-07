895cb64afa2d9ba4bacd7d44f152438c27c03cb9
Jason Siefken <siefkenj@gmail.com>
2022-03-06 22:36:22 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # pretext-react-compiled-article
+
 A compiled version of the Pretext sample article for use in testing pretext-react
+
+This pre-compiled build is used by `pretext-react` for development.
+
+## Building
+
+Install the Pretext CLI and then run
+
+```bash
+./build.sh
+```
+
+This will output the built `sample-article` from Rob Beezer's `overhaul` branch into `html-dev`
+(built with a local reference to the react Javascript) and `html-reference` (built using the
+default Javascript).
---
+++ build.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+mkdir -p tmp
+
+# Grab the latest version of pretext directly from Rob Beezer's branch, since that
+# branch has the features we need.
+wget https://github.com/rbeezer/pretext/archive/refs/heads/overhaul.zip -nc -O tmp/pretext.zip
+
+pushd tmp
+
+rm -rf pretext
+unzip -q pretext.zip
+# We are currenty working with the overhaul branch, but that may change in the future,
+# so we normalize the path name
+mv pretext-overhaul pretext
+
+popd
+
+PRETEXT_BASE_PATH="$PWD/tmp/pretext"
+PRETEXT_HTML_XSL="$PRETEXT_BASE_PATH/xsl/pretext-html.xsl"
+
+set -x
+
+# Compile the source!
+pretext build -x $PRETEXT_HTML_XSL html-dev &
+pretext build -x $PRETEXT_HTML_XSL html-reference &
+
+wait
+
+set +x
+
+echo 'html-dev and html-reference bult'
+echo 'You can view them by running'
+echo '   pretext view html-dev'
+echo ' or'
+echo '   pretext view html-reference'
